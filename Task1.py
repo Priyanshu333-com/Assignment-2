e8d452c20d3d0ab78b704c73e97b5d6abcade844
Priyanshu333-com <priyanshu34920@gmail.com>
2025-12-17 02:13:44 +0530

    Assignment 2

---
+++ Task1.py
@@ -0,0 +1,8 @@
+Number = int(input("Enter a number: "))
+
+if Number % 2 == 0: 
+    print (Number, "is an even number")
+elif Number % 2 != 0 : 
+    print (Number, "is an odd number")
+else :
+    print("Invalid number")
